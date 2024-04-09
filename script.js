2266f6ef22496793a8551b0e803577c395da4d81
Abaz <Abaz95@live.co.uk>
2024-04-09 13:07:10 +0100

    Add functional buttons
    Two buttons added. One which rests the color of all boxes to white and the other which prompts user for a number to change the grid size.
    
    Project is basically complete. Will develop further with more styling and functionality.

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const container = document.getElementById("container")
+const container = document.getElementById("container");
+const resetBtn = document.getElementById("reset-btn");
+const changeGridBtn = document.getElementById("change-grid-btn");
+let userInputOut = 16;
 
+container.addEventListener("DOMContentLoaded", makeGrid(userInputOut));
 
-document.addEventListener("DOMContentLoaded", makeRows);
-
-
-function makeRows() {
-    for (let i = 0; i < 16; i++) {
-        const newDivRow = document.createElement("div")
-        newDivRow.classList.add("rows")
+function makeGrid(userInputOut) {
+    for (let i = 0; i < userInputOut; i++) {
+        const newDivRow = document.createElement("div");
+        newDivRow.classList.add("rows");
         container.appendChild(newDivRow);
 
-        for(let i = 0; i<16; i++) {
-        const newDivColumn = document.createElement("div")
-        newDivColumn.classList.add("columns")
-        newDivRow.appendChild(newDivColumn) 
-        }
-    }
-}
+        for (let i = 0; i < userInputOut; i++) {
+            const newDivColumn = document.createElement("div");
+            newDivColumn.classList.add("columns");
+            newDivRow.appendChild(newDivColumn);
 
+            newDivColumn.addEventListener("mousedown", function () {
+            newDivColumn.style.backgroundColor = "red";
+
+                resetBtn.addEventListener("mousedown", function () {
+                newDivColumn.style.backgroundColor = "white";
+                });
+            });
+        };
+    };
+};
+
+changeGridBtn.addEventListener ("click", function () {
+    let userInputIn = prompt("select a number between 1 and 100");
+    userInputOut = userInputIn; 
+    container.innerHTML = "";
+    makeGrid(userInputOut);
+});
