8fd986e4a7673bbb748ab04e027c586973e1a41c
Abaz <Abaz95@live.co.uk>
2024-04-30 18:43:47 +0100

    Refactor box colouring function
    Now whent the mosue left-click button is held and
    mouse is moved over boxes, they will change colour
    to red.

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const resetBtn = document.getElementById("reset-btn");
 const changeGridBtn = document.getElementById("change-grid-btn");
 let userInputOut = 16;
 
+let newDivRow;
+let newDivColumn;
+let holdInterval;
+let isMouseDown = false;
+
 container.addEventListener("DOMContentLoaded", makeGrid(userInputOut));
 
 function makeGrid(userInputOut) {
     for (let i = 0; i < userInputOut; i++) {
-        const newDivRow = document.createElement("div");
+        newDivRow = document.createElement("div");
         newDivRow.classList.add("rows");
         container.appendChild(newDivRow);
 
@@ -16,16 +21,40 @@ function makeGrid(userInputOut) {
             newDivColumn.classList.add("columns");
             newDivRow.appendChild(newDivColumn);
 
+            // draw
             newDivColumn.addEventListener("mousedown", function () {
-            newDivColumn.style.backgroundColor = "red";
-
-                resetBtn.addEventListener("mousedown", function () {
-                newDivColumn.style.backgroundColor = "white";
-                });
+                isMouseDown = true;
+                holdInterval = setInterval(colorBox, 100);       
             });
+
+            newDivColumn.addEventListener("mouseover", function () {
+                if (isMouseDown) {
+                    colorBox();
+                }                
+            });
+            newDivColumn.addEventListener("mouseup", function () {
+                isMouseDown= false;
+                clearInterval(holdInterval);
+                 
+            });
+
+            function colorBox (){
+                if (isMouseDown){
+                newDivColumn.style.backgroundColor = "red";
+                }
+            }
+
+            // clear drawing
+            resetBtn.addEventListener("mousedown", function () {
+                newDivColumn.style.backgroundColor = "white";
+            });
+            
         };
     };
 };
+
+
+
 
 changeGridBtn.addEventListener ("click", function () {
     let userInputIn = prompt("select a number between 1 and 100");
