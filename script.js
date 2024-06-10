624949b838f4eec38e421a8a0d1e971050b99464
Abaz <Abaz95@live.co.uk>
2024-06-10 17:46:00 +0100

    Add option for random colours
    When checkbox is checked, each time mouse clicked
    or held and passed over boxes, each box fills
    with different colour.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const container = document.getElementById("container");
 const resetBtn = document.getElementById("reset-btn");
 const changeGridBtn = document.getElementById("change-grid-btn");
+const randomColourBtn = document.getElementById("random-colour-btn");
+
 let userInputOut = 16;
-
-
 let holdInterval;
 let isMouseDown = false;
+let randomColourBtnDown = false;
 
-container.addEventListener("DOMContentLoaded", makeGrid(userInputOut));
+document.addEventListener("DOMContentLoaded", makeGrid(userInputOut));
 
 function makeGrid(userInputOut) {
     for (let i = 0; i < userInputOut; i++) {
@@ -18,48 +19,79 @@ function makeGrid(userInputOut) {
         for (let i = 0; i < userInputOut; i++) {
             const newDivColumn = document.createElement("div");
             newDivColumn.classList.add("columns");
-            newDivRow.appendChild(newDivColumn);
-
-            // draw
-            newDivColumn.addEventListener("mousedown", function () {
-                isMouseDown = true;
-                holdInterval = setInterval(colorBox, 100);       
-            });
-
-            newDivColumn.addEventListener("mouseover", function () {
-                if (isMouseDown) {
-                    colorBox();
-                }                
-            });
-            newDivColumn.addEventListener("mouseup", function () {
-                isMouseDown= false;
-                clearInterval(holdInterval);
-                 
-            });
-
-            function colorBox (){
-                if (isMouseDown){
-                newDivColumn.style.backgroundColor = "red";
-                }
-            }
-
-            // clear drawing
-            resetBtn.addEventListener("mousedown", function () {
-                newDivColumn.style.backgroundColor = "white";
-            });
+            newDivRow.appendChild(newDivColumn);         
             
         };
     };
 };
 
+// draw
+container.addEventListener("mousedown", function (event) {
+    if (event.target.classList.contains("columns")) {
+    isMouseDown = true;
+    colorBox(event.target)
+    holdInterval = setInterval(colorBox(event.target), 100);
+    }       
+});
+
+container.addEventListener("mouseover", function (event) {
+    if (isMouseDown && event.target.classList.contains("columns")) {
+        colorBox(event.target);
+    }                
+});
+
+document.addEventListener("mouseup", function () {
+    isMouseDown= false;
+    clearInterval(holdInterval);
+     
+});
+
+function colorBox (element){
+    if (isMouseDown && randomColourBtnDown === true){
+
+        const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`
+        
+        element.style.backgroundColor = rndCol;
+    }
+    else if (isMouseDown) {
+        element.style.backgroundColor = "red";
+    }
+
+}
 
 
- 
+// random color
+function random (number) {
+    return Math.floor(Math.random()*number +1)
+}
 
 
+randomColourBtn.addEventListener("change", function () {
+
+    if(this.checked) {
+        randomColourBtnDown = true;
+
+    }
+    else {
+        randomColourBtnDown = false;
+    }
+})
+
+// clear drawing
+resetBtn.addEventListener("mousedown", function () {
+    const allColumns = document.querySelectorAll(".columns");
+    allColumns.forEach(column => column.style.backgroundColor = "white");
+});
+
+// change number of boxes in grid
 changeGridBtn.addEventListener ("click", function () {
     let userInputIn = prompt("select a number between 1 and 100");
     userInputOut = userInputIn; 
     container.innerHTML = "";
     makeGrid(userInputOut);
 });
+
+
+
+
+
